b7f0be3b4df9fc08f1ef40daa901355fecd5747e
goncalveshigino <goncalveshigino20@gmail.com>
2023-09-21 13:31:47 +0100

    Initial Commit

---
+++ PagarDividas/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PagarDividas
+//
+//  Created by Goncalves Higino on 21/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PagarDividas/PagarDividasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagarDividasApp.swift
+//  PagarDividas
+//
+//  Created by Goncalves Higino on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PagarDividasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
